e48ff6d13135da2a45da34ca2f5676756e410ee4
dsh91698 <mettus@mail.ru>
2022-09-29 15:10:32 +0400

    feat: basic components create

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './header/search/search.component';
+import { LoginComponent } from './header/login/login.component';
+import { SortingComponent } from './header/sorting/sorting.component';
+import { CardVideoComponent } from './main/card-video/card-video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SearchComponent,
+    LoginComponent,
+    SortingComponent,
+    CardVideoComponent,
   ],
   imports: [
     BrowserModule,
