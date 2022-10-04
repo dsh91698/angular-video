9a4b875d096bc5dd41ab090d269b32d727f872af
dsh91698 <mettus@mail.ru>
2022-10-04 10:33:42 +0400

    fix: all components remove except header

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { SearchComponent } from './header/search/search.component';
-import { LoginComponent } from './header/login/login.component';
-import { SortingComponent } from './header/sorting/sorting.component';
-import { CardVideoComponent } from './main/card-video/card-video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
-    SearchComponent,
-    LoginComponent,
-    SortingComponent,
-    CardVideoComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,3 +18,4 @@ import { CardVideoComponent } from './main/card-video/card-video.component';
   bootstrap: [AppComponent],
 })
 export class AppModule { }
+ 
