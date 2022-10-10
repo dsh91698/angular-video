8f4aa113e426ecada2897fdec441996579450741
dsh91698 <mettus@mail.ru>
2022-10-10 18:23:55 +0400

    feat: hide/show search-by section  add

--- youtube-client/src/app/header/header.component.ts
+++ youtube-client/src/app/header/header.component.ts
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isShown_sorting_by__wrapper = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showOrHideSearchBySection(event?:any) {
+    console.log('event->',event,this.isShown_sorting_by__wrapper);
+    this.isShown_sorting_by__wrapper = !this.isShown_sorting_by__wrapper;
   }
 
 }
