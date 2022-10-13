8bb7e232360f6300c4df0269dcb01677bf19e2fd
dsh91698 <mettus@mail.ru>
2022-10-13 23:07:38 +0400

    feat: directive get date-time work - add

--- youtube-client/src/app/directives/time-style.directive.ts
+++ youtube-client/src/app/directives/time-style.directive.ts
@@ -5,30 +5,48 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 })
 export class TimeStyleDirective {
 
-  // @Input() public publishedAt:string = '';
-  @Input('appTimeStyle') publishedAt:string = '';
+  @Input() public publishedAt:string = '';
+  // @Input('appTimeStyle') publishedAt:string = '';
 
   private color:string = 'red';
 
-  // rez.sort((a, b) => (new Date(a.snippet.publishedAt).getTime()) - (new Date(b.snippet.publishedAt).getTime())); // desc sort
+  private postDate:number = new Date().getTime();
+
+  private dateOffset7d: number = (24 * 60 * 60 * 1000) * 7; //7 days
+
+  private dateOffset30d: number = (24 * 60 * 60 * 1000) * 30; //30 days
+
+  private dateOffset6M: number = (24 * 60 * 60 * 1000) * 30 * 6; //6 month
+
+  private nowDate:number = new Date().getTime(); // today
   // If older than 6 months – Red
-  // private postDate:Date = new Date(this.publishedAt);
-
-  private nowDate:Date = new Date();
-
   // If from a month up to 6 months – Yellow
   // If from seven days up to a month – Green
   // If younger than 7 days – Blue
 
-  constructor(private el: ElementRef, private r: Renderer2) {
-    console.log(
-      // 'directive->', el.nativeElement, 
-      'published->', this, 
-      this.publishedAt,
-      'nowDate->', this.nowDate,
-      );
-    this.r.setStyle(this.el.nativeElement, 'background-color', this.color);
+  constructor(private el: ElementRef, private r: Renderer2) {}
 
+  ngOnInit(): any {
+    this.postDate = new Date(this.publishedAt).getTime(); // post publish time
+    let diff: number = this.nowDate - this.postDate;
+
+    if (this.dateOffset7d > diff) {
+      console.log('7d', this.dateOffset7d, diff);
+      this.r.setStyle(this.el.nativeElement, 'background-color', 'blue');
+      return;
+    }
+    if (this.dateOffset7d < diff && this.dateOffset30d > diff) {
+      console.log('30d', this.dateOffset7d, diff);
+      this.r.setStyle(this.el.nativeElement, 'background-color', 'green');
+      return;
+    }
+    if (this.dateOffset30d < diff && this.dateOffset6M > diff) {
+      console.log('6Md', this.dateOffset7d, diff);
+      this.r.setStyle(this.el.nativeElement, 'background-color', 'yellow');
+      return;
+    }
+    
+    this.r.setStyle(this.el.nativeElement, 'background-color', this.color);
   }
 
 }
