7d20d49f25a5604501289f89c34cc131fed0a8f0
dsh91698 <mettus@mail.ru>
2022-10-17 14:55:33 +0400

    feat: auth/login  component create

--- youtube-client/src/app/app-routing.module.ts
+++ youtube-client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { Page404Component } from './core/components/page404/page404.component';
+import { SearchResultsBlockComponent } from './search-results-block/search-results-block.component';
 
 const routes: Routes = [
   //routes
-  {path: '', component: AppComponent},
-  {path: '**', component: AppComponent}
+  // { path: '', component: AppComponent },
+  { path: '', component: SearchResultsBlockComponent },
+  {path: 'login', component: LoginComponent},
+  { path: '**', component: Page404Component },
 ];
 
 @NgModule({
--- youtube-client/src/app/core/core.module.ts
+++ youtube-client/src/app/core/core.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { Page404Component } from './page404/page404.component';
+import { Page404Component } from './components/page404/page404.component';
 
 
 @NgModule({
   declarations: [
-    Page404Component
+    Page404Component,
   ],
   imports: [
     CommonModule,
-    CoreRoutingModule
-  ]
+    CoreRoutingModule,
+  ],
+  exports: [
+    Page404Component,
+  ],
 })
 export class CoreModule { }
