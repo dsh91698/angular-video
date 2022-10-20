b5a64929385788c914a5ea1ce9e036ce8474e1dc
dsh91698 <mettus@mail.ru>
2022-10-20 10:26:35 +0400

    feat: resppnce ar into data-service add

--- youtube-client/src/app/core/services/data.service.ts
+++ youtube-client/src/app/core/services/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { IYouTubeItem } from 'src/app/models/IYouTubeItem';
+import { mockResponse } from '../../response';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
 
+  //vars
+  public response: IYouTubeItem[] = mockResponse.items;
+
+
   constructor() { }
+
+  //methods
 }
