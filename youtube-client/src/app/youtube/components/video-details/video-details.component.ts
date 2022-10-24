722965bc47a16805904284358d076eb193f392f4
dsh91698 <mettus@mail.ru>
2022-10-24 21:52:37 +0400

    feat: video card detail - some features add in design

--- youtube-client/src/app/youtube/components/video-details/video-details.component.ts
+++ youtube-client/src/app/youtube/components/video-details/video-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// import { filter } from 'rxjs';
+import { DataService } from 'src/app/core/services/data.service';
+import { IYouTubeItem } from '../../../models/IYouTubeItem';
+ 
 
 @Component({
   selector: 'app-video-details',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoDetailsComponent implements OnInit {
 
-  constructor() { }
+  public videoId = '';
+  public videoIdtype = '';
+  public videoData!: IYouTubeItem[];
+  public card!: IYouTubeItem;
+
+  constructor(
+    private route: ActivatedRoute, 
+    public dataService: DataService
+    ) { }
+
 
   ngOnInit(): void {
+    this.videoId = this.route.snapshot.params['id'];
+    this.videoData = this.dataService.response;
+    this.card = this.videoData.filter( card => card.id === this.route.snapshot.params['id'])[0];
   }
 
+
+  
 }
