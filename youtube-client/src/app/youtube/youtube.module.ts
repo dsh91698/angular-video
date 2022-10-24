466a1e1bd68ebf83549a1f3f31e828404bf62e55
dsh91698 <mettus@mail.ru>
2022-10-24 18:09:46 +0400

    feat: video-detail component add

--- youtube-client/src/app/youtube/youtube.module.ts
+++ youtube-client/src/app/youtube/youtube.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from '../shared/shared.module';
 import { YoutubeRoutingModule } from './youtube-routing.module';
 import { SearchResultsBlockComponent } from './components/search-results-block/search-results-block.component';
 import { SearchResultItemComponent } from './components/search-results-block/search-result-item/search-result-item.component';
+import { VideoDetailsComponent } from './components/video-details/video-details.component';
 
 
 @NgModule({
   declarations: [
     SearchResultsBlockComponent,
     SearchResultItemComponent,
+    VideoDetailsComponent,
   ],
   imports: [
     CommonModule,
