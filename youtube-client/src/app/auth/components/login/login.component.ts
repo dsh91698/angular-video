0802bb80818f4dc753f86719ba7dfbccd46b4f0b
dsh91698 <mettus@mail.ru>
2022-10-30 21:27:58 +0400

    feat: custom validator add

--- youtube-client/src/app/auth/components/login/login.component.ts
+++ youtube-client/src/app/auth/components/login/login.component.ts
@@ -32,6 +32,7 @@ export class LoginComponent implements OnInit {
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(8),
+        this.isPasswordStrong,
         // Validators.maxLength(30),
       ]),
     });
@@ -44,4 +45,18 @@ export class LoginComponent implements OnInit {
   }
 
   get password() { return this.loginForm.get('password'); }
+
+  public isPasswordStrong(password: FormControl): { [key: string]: boolean } | null {
+    let hasSpecial = /[$@$!%*?&_-|~]/.test(password.value);
+    let hasNumber = /\d/.test(password.value);
+    let hasUpper = /[A-Z]/.test(password.value);
+    let hasLower = /[a-z]/.test(password.value);
+    const isValid = hasNumber && hasUpper && hasLower && hasSpecial;
+    if (!isValid) {
+      console.log(password.value);
+      return { isNotStrong: true };
+    }
+
+    return null;
+  }
 }
