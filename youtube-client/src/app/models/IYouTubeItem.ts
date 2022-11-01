e9405636ad34c9f92ef95b495c19efcf8e5893d1
dsh91698 <mettus@mail.ru>
2022-11-02 01:59:00 +0400

    feat: search working, switch to RapidAPI; new interface add

--- youtube-client/src/app/models/IYouTubeItem.ts
+++ youtube-client/src/app/models/IYouTubeItem.ts
@@ -2,7 +2,7 @@ import { IVideoStatistics } from './IVideoStatistics';
 import { IVideoSnippet } from './IVideoSnippet';
 
 export interface IYouTubeItem {
-  kind: string,
+  kind?: string,
   etag: string,
   id: { 
     kind?: string,
@@ -12,3 +12,25 @@ export interface IYouTubeItem {
   statistics: IVideoStatistics,
 }
   
+
+export interface IRapidApiResponceWithStatistics {
+  kind: string,
+  etag: string,
+  items: [
+    {
+      kind: string,
+      etag: string,
+      id: string,
+      statistics: {
+        viewCount: string,
+        likeCount: string,
+        favoriteCount: string,
+        commentCount: string
+      }
+    },
+  ],
+  pageInfo: {
+    totalResults: number,
+    resultsPerPage: number
+  }
+}
--- youtube-client/src/app/youtube/components/search-results-block/search-results-block.component.ts
+++ youtube-client/src/app/youtube/components/search-results-block/search-results-block.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { debounceTime } from 'rxjs/operators';
 import { DataService } from 'src/app/core/services/data.service';
 import { IYouTubeResponse } from 'src/app/models/you-tube-response';
-import { IYouTubeItem } from '../../../models/IYouTubeItem';
+import { IRapidApiResponceWithStatistics, IYouTubeItem } from '../../../models/IYouTubeItem';
 
 @Component({
   selector: 'app-search-results-block',
@@ -29,16 +29,53 @@ export class SearchResultsBlockComponent implements OnInit {
       debounceTime(1500),
     ).subscribe(
 
-      val => {
-        console.log('res->', val);
-        let re = this.http.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBTeKuESi66BKPmR2XC8f6asXINeVSgeYU&type=video&part=snippet&maxResults=5&q=${val}`);
+      searchPhrase => {
+        console.log('res->', searchPhrase);
+        // let re = this.http.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBTeKuESi66BKPmR2XC8f6asXINeVSgeYU&type=video&part=snippet&maxResults=5&q=${val}`);        
+        let re = this.http.get(
+          `https://youtube-v31.p.rapidapi.com/search?&type=video&part=snippet&maxResults=5&q=${searchPhrase}`, {
+            headers: { 'X-RapidAPI-Key': '0519fa2feamsh1b49deb43e0d50ep186198jsnc1907a46938d' },
+          });        
+        
         re.subscribe(
           r => {
             console.log('response youtube->', r);
-            this.dataService.response = (r as IYouTubeResponse).items;
-            this.response = (r as IYouTubeResponse).items;
+
+            let ids: string[] = (r as IYouTubeResponse).items.map(el => el.id.videoId); // videoId's array 
+            console.log('ids youtube->', ids);
+
+            // let re2 = this.http.get(`https://youtube.googleapis.com/youtube/v3/videos?id=${ids.join('=')}&maxResults=5&key=AIzaSyBTeKuESi66BKPmR2XC8f6asXINeVSgeYU&&part=statistics`);
+            let re2 = this.http.get(
+              `https://youtube-v31.p.rapidapi.com/videos?part=snippet%2Cstatistics&id=${ids.join(',')}`, {
+                headers: { 'X-RapidAPI-Key': '0519fa2feamsh1b49deb43e0d50ep186198jsnc1907a46938d' },
+              });
+
+            re2.subscribe(
+              re22 => {
+                console.log('re2->', re22);
+              
+                let resp = (r as IYouTubeResponse).items.map(rel => {
+                  const match = (re22 as IRapidApiResponceWithStatistics).items.find(element => element.id === rel.id.videoId);
+                  if (match) {
+                    return { ...match, ...rel };
+                  } else { return; }
+                });
+
+                console.log('resp->', resp);
+
+
+                this.dataService.response = (resp as IYouTubeItem[]);
+                this.response = (resp as IYouTubeItem[]);
+    
+
+              },
+  
+            );
+
+            
+            // this.dataService.response = (r as IYouTubeResponse).items;
+            // this.response = (r as IYouTubeResponse).items;
           },
-          
         );
           
       },
