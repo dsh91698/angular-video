0ac348647263c2dd00f373bd93b3b94551cf2c87
dsh91698 <mettus@mail.ru>
2022-11-02 23:27:24 +0400

    feat: adminPage implemented

--- youtube-client/src/app/admin/components/admin-page/admin-page.component.ts
+++ youtube-client/src/app/admin/components/admin-page/admin-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AdminPageValidators } from './admin-validators';
+
 
 @Component({
   selector: 'app-admin-page',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
+  public adminForm: FormGroup;
 
-  ngOnInit(): void {
+  public inputDate: Date = new Date();
+
+
+  constructor() { 
+    this.adminForm = new FormGroup({
+    });
+
   }
 
-}
+  ngOnInit(): void {
+
+    this.adminForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required, //Please enter a title
+        Validators.minLength(3), //The title is too short
+        Validators.maxLength(20), //The title is too long
+      ]),
+
+      description: new FormControl(null, [
+        Validators.maxLength(255), //The description is too long
+      ]),
+
+      imgCover: new FormControl(null, [
+        Validators.required, //Please enter a link to the image
+        AdminPageValidators.isUrlCorrect, //The image link is invalid
+      ]),
+
+      videoLink: new FormControl(null, [
+        Validators.required, //Please enter a link to the video
+        AdminPageValidators.isUrlCorrect, //The video link is invalid
+      ]),
+
+      creationDate: new FormControl(null, [
+        Validators.required, //Please enter a creation date
+        AdminPageValidators.isDateCorrect, //The date is invalid
+      ]),
+    });
+
+  }
+
+  submitForm() {
+    console.log('submit adminForm...', this.adminForm.value);
+  }
+
+
+} 
---
+++ youtube-client/src/app/admin/components/admin-page/admin-validators.ts
@@ -0,0 +1,35 @@
+// admin page castom validators
+import { FormControl } from '@angular/forms';
+
+export class AdminPageValidators {
+
+  public static isUrlCorrect(Url: FormControl): { [key: string]: boolean } | null {
+    try {
+      new URL(Url.value);
+      return null;
+    } catch (err) {
+      return { isNotCorrectUrl: true };
+    }
+  }
+
+  public static isDateCorrect(createDate: FormControl): { [key: string]: boolean } | null {
+    // future date check
+    let today: number = new Date().getTime(); // ms
+    let videoCreationDate: number = new Date(createDate.value).getTime(); // ms
+    let isDateInFuture = videoCreationDate > today;
+    if (isDateInFuture) {
+      return { isDateInFuture: true };
+    }
+
+    // correct date check
+    let isDateValid = new Date(createDate.value);
+    console.log('isValid date url ->', isDateValid);
+    console.log('createDate value ----->', createDate.value);
+    if (isDateValid.toString() === 'Invalid Date') {
+      return { isNotCorrectDate: true };
+    }
+    return null;
+  }
+    
+}
+
